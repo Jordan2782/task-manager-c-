7eac65a089a1ec2e141eaf0b38214f1d85ac4dfd
Jordan2782 <j.kemadjou27@gmail.com>
2026-07-01 23:23:38 +0200

    Create task.hpp

---
+++ include/task.hpp
@@ -0,0 +1,13 @@
+#ifndef TASK_HPP_INCLUDED
+#define TASK_HPP_INCLUDED
+#include <iostream>
+class Task
+{
+public:
+
+    std::string nameOfTask;
+     int taskDuration;
+    bool isFinish;
+};
+
+#endif // TASK_HPP_INCLUDED
