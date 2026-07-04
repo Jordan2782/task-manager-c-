ba75dececf2d632f2dd3a62eb023fce49d79cabe
Jordan2782 <j.kemadjou27@gmail.com>
2026-07-04 23:18:36 +0200

    Update main.cpp

--- src/Main/main.cpp
+++ src/Main/main.cpp
@@ -1,10 +1,61 @@
 #include <iostream>
-
+#include "../../include/taskManager.hpp"
 using namespace std;
+#include <string>
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    TaskManager taskManager{};
+
+   int reponse = 0;
+
+
+    while(reponse !=4)
+    {
+        cout << "Task Manager " << endl;
+        cout << "1. add a Task " << endl;
+       cout << "2. show the list Tasks " << endl;
+       cout << "3. Mark a Task at Finish  " << endl;
+       cout << "4. quit " << endl;
+       cin >> reponse;
+
+
+
+
+   if (reponse == 1)
+   {
+        cout << "enter the name of Task and his Duration " << endl;
+        string name;
+        cin.ignore();
+        getline(cin,name );
+        int duration;
+        cin>> duration;
+        Task task {name,duration};
+        taskManager.addTask(task);
+}
+
+
+if (reponse == 2)
+   {
+        cout << "list of Task  " << endl;
+        taskManager.readTask();
+}
+
+if (reponse == 3)
+   {
+        cout << "Mark a Task as Finish   " << endl;
+        int pos;
+        cout << " Enter the Position of the Task" << endl;
+        cin>>pos;
+
+        taskManager.markedAsFinished(pos);
+}
+
+
+}
+
+
+        cout << "Programm Finish  " << endl;
 
 
     return 0;
