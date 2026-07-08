9b93a49ae409d4a2a5421fc59093ae4af269d5ff
Jordan2782 <j.kemadjou27@gmail.com>
2026-07-08 21:03:24 +0200

    Update task.cpp

--- src/Main/task.cpp
+++ src/Main/task.cpp
@@ -1,6 +1,6 @@
 #include "../../include/task.hpp"
 #include <string>
-Task::Task(const std::string &nameOfTask,int taskDuration, bool isFinished)
+Task::Task(const std::string &nameOfTask,const int taskDuration, bool isFinished)
 :nameOfTask{nameOfTask}, taskDuration{taskDuration} ,isFinished{isFinished}
 {
 
